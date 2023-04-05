136c87b3cb12e756544e07c8355173ae1c3b0c3a
Ansh Gupta <38015438+anshgupta1234@users.noreply.github.com>
2023-04-05 11:53:22 -0400

    Update build.py

--- toolchain/mfc/build.py
+++ toolchain/mfc/build.py
@@ -144,7 +144,9 @@ def build_target(name: str, history: typing.List[str] = None):
     flags: list = target.flags.copy() + [
         # Disable CMake warnings intended for developers (us).
         # See: https://cmake.org/cmake/help/latest/manual/cmake.1.html.
-        f"-Wno-dev",
+        f"-Wdev",
+        f"--debug-output",
+        f"--trace",
         # Save a compile_commands.json file with the compile commands used to
         # build the configured targets. This is mostly useful for debugging.
         # See: https://cmake.org/cmake/help/latest/variable/CMAKE_EXPORT_COMPILE_COMMANDS.html.
